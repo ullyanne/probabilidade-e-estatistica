1eba19a54279c1a901453f44d6ecdf13ef5ae1ff
ullyanne <ujfp@ic.ufal.br>
2021-11-20 18:24:12 -0300

    update question 1.1 and 1.2

---
+++ question_1-1.R
@@ -0,0 +1,39 @@
+df <- as.data.frame(dados_enem)
+
+notas.tb <- (df$NOTA_ENEN)
+View(notas.tb)
+
+#quartis
+classes <- c("300.02-450.06", "450.06-493.96", "493.96-550.28", "550.28-796.14")
+notas.cut = table(cut(df$NOTA_ENEN, breaks=quantile(df$NOTA_ENEN, na.rm=T), include.lowest = TRUE, labels=classes))
+
+#----Boxplot
+boxplot(df$NOTA_ENEN, col = "light blue", main="Boxplot - Notas", ylab="Notas")
+
+#---Frequência relativa
+#relfreq <- notas.tb/sum(notas.cut)
+relfreq <- prop.table(notas.cut)
+
+#---Frequência absoluta
+absfreq <- table(notas.tb)
+View(absfreq)
+
+#---Frequência relativa acumulada
+relfreq_a <- cumsum(relfreq)
+
+#Frequência absoluta acumulada
+absfreq_a <- cumsum(notas.cut)
+
+#----Tabela das frequências
+frequencies = cbind(notas.cut, absfreq_a, 
+                    relfreq = round(relfreq*1, digits=2),
+                    relfreq_a=round(relfreq_a*1,digits = 2))
+colnames(frequencies) <- c("Frequência absoluta", "Frequência absoluta acumulada", "Frequência relativa", "Frequência relativa acumulada")
+View(frequencies)
+
+#----Histograma
+hist(notas.tb, col="spring green", ylab="Frequência", xlab="Notas", main="Histograma - Notas")
+
+
+
+
